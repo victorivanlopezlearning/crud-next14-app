fcbbd18c1da820f21ac1891563f5e3428af9792f
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-12-09 17:46:52 -0600

    Project: init

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} dark:bg-slate-900 dark:text-white`}>{children}</body>
     </html>
   )
 }
