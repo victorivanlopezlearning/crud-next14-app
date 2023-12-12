a63ab163a65c3186b8b92e1e4aa18cb1c0018c98
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-12-11 20:33:02 -0600

    Created: endpoints /tasks/[id] GET, PUT, DELETE

---
+++ src/app/api/tasks/[id]/route.js
@@ -0,0 +1,43 @@
+import { NextResponse } from 'next/server';
+import { prisma } from '@/libs/prisma';
+
+export async function GET(request, { params }) {
+
+  const task = await prisma.task.findUnique({
+    where: {
+      id: Number(params.id),
+    },
+  });
+
+  if (!task) return NextResponse.json({ error: 'Tarea no encontrada' }, { status: 404 });
+
+  return NextResponse.json(task);
+}
+
+export async function PUT(request, { params }) {
+
+  const data = await request.json();
+
+  const taskUpdated = await prisma.task.update({
+    where: {
+      id: Number(params.id)
+    },
+    data: data,
+  })
+
+  return NextResponse.json(taskUpdated);
+}
+
+export async function DELETE(request, { params }) {
+  try {
+    const taskDeleted = await prisma.task.delete({
+      where: {
+        id: Number(params.id),
+      },
+    });
+
+    return NextResponse.json(taskDeleted);
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
