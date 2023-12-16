f15ecfd83a48e5988a812804d98749afea531d2f
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-12-15 21:26:56 -0600

    Created: EditTaskPage

--- src/app/tasks/edit/[id]/page.jsx
+++ src/app/tasks/edit/[id]/page.jsx
@@ -1,6 +1,22 @@
+import { FormTask } from "@/components";
+import { redirect } from "next/navigation";
 
-export default function page() {
+async function getTask(id) {
+  const res = await fetch(`http://localhost:3000/api/tasks/${id}`);
+  return await res.json();
+}
+
+export default async function EditTaskPage({ params: { id } }) {
+
+  const task = await getTask(id);
+
+  if (task.error) redirect('/');
+
   return (
-    <div>page</div>
+    <div className="max-w-md mx-auto">
+      <h1 className="text-4xl font-bold text-center mt-10">Editando: {task.name}</h1>
+
+      <FormTask />
+    </div>
   )
 }
