c36f7a8ad117ef21942ac90a86aa75ba69eb12cd
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-12-16 20:49:53 -0600

    Created: updated task functionality

--- src/components/FormTask.jsx
+++ src/components/FormTask.jsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useForm } from '@/hooks';
 
@@ -8,10 +9,20 @@ const initialForm = {
   description: '',
 }
 
-export const FormTask = () => {
-  const { name, description, onInputChange } = useForm(initialForm);
+export const FormTask = ({ task = {} }) => {
+  const { name, description, onInputChange, onUpdateDataForm } = useForm(initialForm);
 
   const router = useRouter();
+
+  useEffect(() => {
+    if (task.id) {
+      onUpdateDataForm({
+        name: task.name,
+        description: task.description,
+      })
+    }
+  }, [])
+
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -22,25 +33,47 @@ export const FormTask = () => {
 
     router.refresh();
 
-    try {
-      const res = await fetch('http://localhost:3000/api/tasks', {
-        method: 'POST',
-        headers: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify({ name, description }),
-      });
+    if (task.id) {
+      try {
+        const res = await fetch(`http://localhost:3000/api/tasks/${task.id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify({ name, description }),
+        });
 
-      if (res.ok) {
-        router.push('/');
-      } else {
-        throw new Error('Error al crear la tarea.');
+        if (res.ok) {
+          router.push('/');
+        } else {
+          throw new Error('Error al actualizar la tarea.');
+        }
+
+      } catch (error) {
+        console.log(error);
       }
+    } else {
+      try {
+        const res = await fetch('http://localhost:3000/api/tasks', {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify({ name, description }),
+        });
 
-    } catch (error) {
-      console.log(error);
+        if (res.ok) {
+          router.push('/');
+        } else {
+          throw new Error('Error al crear la tarea.');
+        }
+
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
+
   return (
     <form
       className="mx-4 md:mx-0 my-20"
@@ -69,12 +102,31 @@ export const FormTask = () => {
           onChange={onInputChange}
         />
       </div>
-      <button
-        type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 transition-colors"
-      >
-        Crear tarea
-      </button>
+
+      <div className='flex justify-between gap-4 md:gap-0'>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 transition-colors"
+        >
+          {
+            task.id
+              ? 'Guardar cambios'
+              : 'Crear tarea'
+          }
+
+        </button>
+
+        {
+          task.id && (
+            <button
+              type="button"
+              className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 transition-colors"
+            >
+              Eliminar
+            </button>
+          )
+        }
+      </div>
     </form>
   )
 }
