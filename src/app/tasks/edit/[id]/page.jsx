60d2bbfdbaeaed0b0ff0bb3ff3a13c9dd72d1871
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-12-16 20:51:35 -0600

    Added: navegation return

--- src/app/tasks/edit/[id]/page.jsx
+++ src/app/tasks/edit/[id]/page.jsx
@@ -1,8 +1,11 @@
-import { FormTask } from "@/components";
+import Link from "next/link";
 import { redirect } from "next/navigation";
+import { FormTask } from "@/components";
 
 async function getTask(id) {
-  const res = await fetch(`http://localhost:3000/api/tasks/${id}`);
+  const res = await fetch(`http://localhost:3000/api/tasks/${id}`, {
+    cache: 'no-store'
+  });
   return await res.json();
 }
 
@@ -16,7 +19,17 @@ export default async function EditTaskPage({ params: { id } }) {
     <div className="max-w-md mx-auto">
       <h1 className="text-4xl font-bold text-center mt-10">Editando: {task.name}</h1>
 
-      <FormTask />
+      <FormTask task={task} />
+
+      <Link
+        href="/"
+        className="flex gap-2 items-center mt-4 hover:underline mx-4 md:mx-0 font-semibold"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+          <path fillRule="evenodd" d="M7.28 7.72a.75.75 0 010 1.06l-2.47 2.47H21a.75.75 0 010 1.5H4.81l2.47 2.47a.75.75 0 11-1.06 1.06l-3.75-3.75a.75.75 0 010-1.06l3.75-3.75a.75.75 0 011.06 0z" clipRule="evenodd" />
+        </svg>
+        <span>Todas las tareas</span>
+      </Link>
     </div>
   )
 }
--- src/app/tasks/new/page.jsx
+++ src/app/tasks/new/page.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { FormTask } from '@/components';
 
 export default function NewTaskPage() {
@@ -6,6 +7,18 @@ export default function NewTaskPage() {
       <h1 className="text-4xl font-bold text-center mt-10">Nueva Tarea</h1>
 
       <FormTask />
+
+      <div className='w-[50%]'>
+        <Link
+          href="/"
+          className="flex gap-2 items-center mt-4 hover:underline mx-4 md:mx-0 font-semibold"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+            <path fillRule="evenodd" d="M7.28 7.72a.75.75 0 010 1.06l-2.47 2.47H21a.75.75 0 010 1.5H4.81l2.47 2.47a.75.75 0 11-1.06 1.06l-3.75-3.75a.75.75 0 010-1.06l3.75-3.75a.75.75 0 011.06 0z" clipRule="evenodd" />
+          </svg>
+          <span>Todas las tareas</span>
+        </Link>
+      </div>
     </div>
   )
 }
